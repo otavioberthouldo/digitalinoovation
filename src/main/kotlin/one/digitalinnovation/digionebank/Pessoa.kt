76e0e23f99b673209781d1c69c865eeec00b8b57
otavioberthouldo <otavio.bsilva18@gmail.com>
2021-08-10 21:05:30 -0300

    classe pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Otávio"
+    var cpf: String = "132.123.123-22"
+}
+fun main() {
+    val otavio = Pessoa()
+
+    println(otavio.nome)
+    println(otavio.cpf)
 }
