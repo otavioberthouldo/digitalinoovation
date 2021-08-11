4a02c3bda23f544beb523f0c8596571cafed69f7
otavioberthouldo <otavio.bsilva18@gmail.com>
2021-08-10 21:48:09 -0300

    atualização para aplicar o modificador privado para definir função de propriedade do CPF

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Otávio"
-    var cpf: String = "132.123.123-22"
+
+    var cpf: String = "108.711.236-22"
+    private set
+
 }
+
 fun main() {
     val otavio = Pessoa()
 
+    println(otavio)
     println(otavio.nome)
     println(otavio.cpf)
+
 }
