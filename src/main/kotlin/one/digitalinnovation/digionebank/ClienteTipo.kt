43ee11f502644eab9b83bd78ec0be74e69763519
otavioberthouldo <otavio.bsilva18@gmail.com>
2021-08-25 23:26:02 -0300

    created  ClienteTipo and TesteClienteTipoEnum

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebank
+/* Uma classe que pode ter um construtor primario*/
+
+enum class ClienteTipo( val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica");
+}
